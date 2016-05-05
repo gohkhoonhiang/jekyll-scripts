66afdfa22b796e9b83a22b94b0c7eee426659d88
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-05-05 20:45:52 +0800

    Fix #1

--- publishpost.sh
+++ publishpost.sh
@@ -5,7 +5,11 @@ for f in $(ls _drafts/); do
     if [[ "$f" == *"$1"* ]]; then
         echo $f
         now=$(date +"%Y-%m-%d")
-        newfilename="$now-$(echo $f | cut -d'-' -f4-)"
+        if [[ "$f" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.* ]]; then 
+            newfilename="$now-$(echo $f | cut -d'-' -f4-)"
+        else
+            newfilename="$now-$f"
+        fi
         echo $newfilename
         mv _drafts/$f _posts/$newfilename
         sed -i "4s/.*.*/date: `date +'%Y-%m-%d %H:%M:%S'` +0800/g" "_posts/$newfilename"
